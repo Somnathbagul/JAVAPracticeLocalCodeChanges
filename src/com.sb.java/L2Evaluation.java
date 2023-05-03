7611c392205525dbc9ed61656bc14647f65964ce
somnath bagul <somnathvbagul@gmail.com>
2023-05-03 13:04:44 +0530

    ctr alt l for formatting in Intellij IDE

--- src/com.sb.java/L2Evaluation.java
+++ src/com.sb.java/L2Evaluation.java
@@ -8,53 +8,53 @@ public class L2Evaluation {
 
     public static void main(String[] args) {
 
-        String sample="abcdeffghiikkjj";
+        String sample = "abcdeffghiikkjj";
         //find second repetative character in sample
-        System.out.println("find second repetative character in sample is "+findSecondRepetativeCharacter(sample));
+        System.out.println("find second repetative character in sample is " + findSecondRepetativeCharacter(sample));
 
-         int number = 5;
+        int number = 5;
         //Program to check if a number is prime or not
-         System.out.println("Number "+number+" Is prime or not? "+ isPrime(number));
+        System.out.println("Number " + number + " Is prime or not? " + isPrime(number));
         //Program to reverse a string
-         System.out.println("String abc and it's reverse string is "+reverseString("abc"));
+        System.out.println("String abc and it's reverse string is " + reverseString("abc"));
         //Program to find the factorial of a number
-         System.out.println("Calculate factorial of given number "+number+" is "+factorial(number));
+        System.out.println("Calculate factorial of given number " + number + " is " + factorial(number));
         //Program to check if a string is a palindrome
         String inputString = "mam";
-         System.out.println("Check the given String as "+inputString+" is palindrome? "+isPalindrome(inputString));
+        System.out.println("Check the given String as " + inputString + " is palindrome? " + isPalindrome(inputString));
         //Program to find the largest and smallest elements in an array
-        int[] inputArray ={1,2,4,5};
+        int[] inputArray = {1, 2, 4, 5};
         findMinMax(inputArray);
 
         //Program to find the second-largest element in an array
-        System.out.println("second-largest number from array "+Arrays.toString(inputArray)+" is: "+findSecondLargest(inputArray));
+        System.out.println("second-largest number from array " + Arrays.toString(inputArray) + " is: " + findSecondLargest(inputArray));
 
         //Program to check if two strings are anagrams
         String str1 = "abc";
         String str2 = "bac";
-        System.out.println("check Strings are anagrams or not?"+areAnagrams(str1,str2));
+        System.out.println("check Strings are anagrams or not?" + areAnagrams(str1, str2));
 
         //program to find the highest element in an array
-       Optional<Integer> highestElement  = Arrays.stream(inputArray).boxed().distinct().sorted(Collections.reverseOrder()).findFirst();
-       System.out.println("highestElement is :"+highestElement.orElse(0)+ " from an input array : "+Arrays.toString(inputArray));
+        Optional<Integer> highestElement = Arrays.stream(inputArray).boxed().distinct().sorted(Collections.reverseOrder()).findFirst();
+        System.out.println("highestElement is :" + highestElement.orElse(0) + " from an input array : " + Arrays.toString(inputArray));
 
         //program to find lowest element in an array
-        Optional<Integer> lowestElement  = Arrays.stream(inputArray).boxed().distinct().sorted().findFirst();
-        System.out.println("lowestElement is :"+lowestElement.orElse(0)+ " from an input array : "+Arrays.toString(inputArray));
+        Optional<Integer> lowestElement = Arrays.stream(inputArray).boxed().distinct().sorted().findFirst();
+        System.out.println("lowestElement is :" + lowestElement.orElse(0) + " from an input array : " + Arrays.toString(inputArray));
 
         //program to find the middle element in an array
-        Optional<Integer> middleElement  = Arrays.stream(inputArray).boxed().distinct().sorted(Collections.reverseOrder()).skip(1).findFirst();
-        System.out.println("middleElement is :"+middleElement.orElse(0)+ " from an input array : "+Arrays.toString(inputArray));
+        Optional<Integer> middleElement = Arrays.stream(inputArray).boxed().distinct().sorted(Collections.reverseOrder()).skip(1).findFirst();
+        System.out.println("middleElement is :" + middleElement.orElse(0) + " from an input array : " + Arrays.toString(inputArray));
 
         //program to check if a number is even or odd
-        System.out.println("Number "+number+" is even or odd? "+isEvenOrOdd(number));
+        System.out.println("Number " + number + " is even or odd? " + isEvenOrOdd(number));
 
 
     }
 
     private static String findSecondRepetativeCharacter(String sample) {
         int index = 0;
-        for (int i = 0; i < sample.length()-1; i++) {
+        for (int i = 0; i < sample.length() - 1; i++) {
             if (sample.charAt(i) == sample.charAt(index)) {
                 index++;
             } else {
@@ -108,7 +108,7 @@ public class L2Evaluation {
                 max = arr[i];
             }
         }
-        System.out.println("Given Input Array is "+Arrays.toString(arr));
+        System.out.println("Given Input Array is " + Arrays.toString(arr));
         System.out.println("Minimum element: " + min);
         System.out.println("Maximum element: " + max);
     }
@@ -129,6 +129,7 @@ public class L2Evaluation {
         }
         return true;
     }
+
     public static String reverseString(String str) {
         return new StringBuilder(str).reverse().toString();
     }
@@ -139,7 +140,6 @@ public class L2Evaluation {
         }
         return n * factorial(n - 1);
     }
-
 
 
 }
